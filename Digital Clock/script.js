49a08b155fa04712e2810292b9b0363568a6544e
Pawan Pal <pawanpal2001@gmail.com>
2023-05-23 22:54:56 +0530

    inserted setInterval method in script and replace p id with span tag in index file and also added some style

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,11 +1,13 @@
 function getTime(){
     var now = new Date();
     var hour = now.getHours();
+    if(hour>12){
+        hour = hour - 12;
+    }
     var minute = now.getMinutes();
     var second = now.getSeconds();
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("minute").innerHTML = minute;
     document.getElementById("second").innerHTML = second;
 }
-setInterval(getTime(), 1000);
-// write a program to display current time that refreshes every second using javascript
+setInterval(getTime, 1000);
